04086a2741cdab5fb44a7c6bf206896f79aaa0d1
Michal Sytko <michal.sytko@gmail.com>
2023-05-18 23:57:52 +0200

    poc init

---
+++ src/BlazorShowroom.App/Dashboard.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorShowroom.App;
+public partial class Dashboard
+{
+    [Parameter]
+    public required string Test { get; set; }
+
+    protected override void OnInitialized()
+    {
+        Test = "12333";
+    }
+}
---
+++ src/BlazorShowroom.App/wwwroot/app.js
@@ -0,0 +1,29 @@
+﻿const storageKey = 'theme-preference'
+
+const getColorPreference = () => {
+    if (localStorage.getItem(storageKey))
+        return localStorage.getItem(storageKey)
+    else
+        return window.matchMedia('(prefers-color-scheme: dark)').matches
+            ? 'dark'
+            : 'light'
+}
+
+const theme = {
+    value: getColorPreference(),
+}
+
+const reflectPreference = () => {
+    document.firstElementChild
+        .setAttribute('data-theme', theme.value)
+}
+
+// set early so no page flashes / CSS is made aware
+reflectPreference()
+
+window.onload = () => {
+    // set on load so screen readers can see latest value on the button
+    reflectPreference()
+
+    document.querySelector('html').style.setProperty('--hue', '200');
+}
