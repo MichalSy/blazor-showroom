03e292d4ddefe3cfc9eda3321c1faf464ff619da
Michal Sytko <michal.sytko@gmail.com>
2023-05-19 19:14:39 +0200

    next update

--- src/BlazorShowroom.App/Dashboard.razor.cs
+++ src/BlazorShowroom.App/Dashboard.razor.cs
@@ -6,8 +6,4 @@ public partial class Dashboard
     [Parameter]
     public required string Test { get; set; }
 
-    protected override void OnInitialized()
-    {
-        Test = "12333";
-    }
 }
---
+++ src/BlazorShowroom.App/wwwroot/app-startup.js
@@ -0,0 +1,43 @@
+﻿function app() {
+    let storageKey = 'theme-preference'
+    let currentTheme = 'light'
+
+    let getColorPreference = () => {
+        if (localStorage.getItem(storageKey)) {
+            return localStorage.getItem(storageKey)
+        }
+
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
+
+    let reflectPreference = () => {
+        document.firstElementChild.setAttribute('data-theme', currentTheme)
+    }
+
+    currentTheme = getColorPreference()
+    reflectPreference()
+
+    window.onload = () => {
+        reflectPreference()
+    }
+
+    // sync with system changes
+    window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', ({ matches: isDark }) => {
+            theme.value = isDark ? 'dark' : 'light'
+            setPreference()
+        })
+
+
+    this.toggleTheme = function () {
+        currentTheme = currentTheme === 'light' ? 'dark' : 'light'
+        localStorage.setItem(storageKey, currentTheme)
+        reflectPreference()
+    }
+
+    this.setThemeColor = function (hueValue) {
+        document.querySelector('html').style.setProperty('--hue', hueValue);
+    }
+}
+window.app_instance = new app()
